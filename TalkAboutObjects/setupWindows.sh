ac6a2d5bbcd5673ac9ca3a8ce675ad9ee23d23e5
jasonjasontu <jasonpsu@163.com>
2016-12-22 17:31:07 -0500

    Update setupWindows.sh

--- TalkAboutObjects/setupWindows.sh
+++ TalkAboutObjects/setupWindows.sh
@@ -10,8 +10,8 @@ pip install packages/decorator-4.0.10-py2.py3-none-any.whl
 pip install packages/Django-1.10.3-py2.py3-none-any.whl
 pip install packages/nltk-3.2.1-py2.py3-none-any.whl
 pip install packages/python_dateutil-2.6.0-py2.py3-none-any.whl
-pip install packages/scikit_learn-0.18.1-cp27-cp27m-win_amd64.whl
-pip install packages/numpy-1.11.2+mkl-cp27-cp27m-win_amd64.whl
+pip install scikit_learn-0.18.1-cp27-cp27m-win_amd64.whl
+pip install numpy-1.11.2+mkl-cp27-cp27m-win_amd64.whl
 pip install packages/scipy-0.18.1-cp27-cp27m-win_amd64.whl
 pip install packages/sklearn_contrib_lightning-0.4.0-cp27-cp27m-win_amd64.whlpip install packages/virtualenv-15.1.0-py2.py3-none-any.whl
 pip install utils
